ebd8602eeb2e47d73369e430a11c7024a3b2dcd7
Pikeruman-f18 <wataru.miyaji@gmail.com>
2020-01-26 19:27:49 +0900

    新・明解C++入門_乱数生成

---
+++ Tutorial/rand.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+
+int main()
+{
+    //乱数の種を生成する
+    srand(time(NULL));
+
+    //0~9の乱数を生成する
+    int lucky = rand() % 10; 
+
+    printf("今日のラッキーナンバーは【%d】です！\n", &lucky);
+
+}
