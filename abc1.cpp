4f6e2d05872eae6557985c68c6b6a81a5501d2c4
Pikeruman-f18 <wataru.miyaji@gmail.com>
2020-02-04 00:13:07 +0900

    20200204 AtCoder153 A〜C 回答

--- abc1.cpp
+++ abc1.cpp
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rep(i,n) for (int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
 int main()
 {
+    int H,A;
 
+    cin>>H>>A;
 
-    retunr 0;
+    int ans = (H+A-1)/A;
+    cout<<ans<<endl;
+    
+    return 0;
 }
--- abc2.cpp
+++ abc2.cpp
@@ -1,37 +1,16 @@
-#include <stdio.h>
-#include <vector>
-#include <iostream>
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
-
-int main ()
-{
-    int H, N;
-
-    scanf("%d %d", &H, &N);
-
-    vector<int> vec(N);
-    for (int i = 0; i < N; i++) {
-        cin >> vec.at(i);
-    }
-    
-    for(int j=0; j<N; j++)
-    {
-        if (H > 0)
-        {
-            H = H - vec.at(j);
-        }
-        else
-        {
-            printf("Yes\n");
-            break;
-        }
-
-    }
-
-    if(H<=0)
-    {
-        printf("No\n");
-    }
-    
-    return 0;
+using ll = long long;
+using P = pair<int,int>;
+ 
+int main() {
+  int h,n;
+  cin >> h >> n;
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
+  rep(i,n) h -= a[i];
+  if (h <= 0) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
 }
