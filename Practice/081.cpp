2ee9559cb2391dec6246f7259913e37b3c012b10
Pikeruman-f18 <wataru.miyaji@gmail.com>
2020-02-15 19:04:36 +0900

    081.cpp commit

--- Practice/081.cpp
+++ Practice/081.cpp
@@ -7,6 +7,27 @@ using P = pair<int,int>;
 int main()
 {
 
+    string s;
+    int cnt = 0;
+
+    cin>>s;
+
+    if(s[0] == '1')
+    {
+        cnt++;
+    }
+
+    if(s[1] == '1')
+    {
+        cnt++;
+    }
+
+    if(s[2] == '1')
+    {
+        cnt++;
+    }
+
+    cout<<cnt<<endl;
 
     return 0;
 }
